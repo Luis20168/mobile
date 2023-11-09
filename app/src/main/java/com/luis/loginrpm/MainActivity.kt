04c8260a513376f9b38d3b2abf18d0f200c0b4c4
Luisito <martinesj405@gmail.com>
2023-11-09 14:34:33 -0500

    splash

--- app/src/main/java/com/luis/loginrpm/MainActivity.kt
+++ app/src/main/java/com/luis/loginrpm/MainActivity.kt
@@ -3,14 +3,23 @@ package com.luis.loginrpm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.luis.loginrpm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        val screenSplash= installSplashScreen()
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+        screenSplash.setKeepOnScreenCondition{false}
+
+
+
 
         binding.btnLogin.setOnClickListener {
             var name= binding.email.text.toString()
